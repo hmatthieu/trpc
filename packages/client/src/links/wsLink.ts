01406db23c15a9994dcc447d121e151e022736ed
Alexander Johansson <alexander@n1s.se>
2025-01-02 17:07:16 +0100

    fix rollup config

--- packages/client/src/links/wsLink.ts
+++ packages/client/src/links/wsLink.ts
@@ -3,14 +3,14 @@ import type {
   AnyRouter,
   inferClientTypes,
 } from '@trpc/server/unstable-core-do-not-import';
-import type { TransformerOptions } from '../../unstable-internals';
-import { getTransformer } from '../../unstable-internals';
-import type { TRPCLink } from '../types';
+import type { TransformerOptions } from '../unstable-internals';
+import { getTransformer } from '../unstable-internals';
+import type { TRPCLink } from './types';
 import type {
   TRPCWebSocketClient,
   WebSocketClientOptions,
-} from './createWsClient';
-import { createWSClient } from './createWsClient';
+} from './wsLink/createWsClient';
+import { createWSClient } from './wsLink/createWsClient';
 
 export type WebSocketLinkOptions<TRouter extends AnyRouter> = {
   client: TRPCWebSocketClient;
